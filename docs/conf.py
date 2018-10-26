d8727cd1dd72726f99c3982ae12f1f3ed29652d1
Jia Chunying <iccccing@gmail.com>
2018-10-26 22:37:35 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -191,7 +191,7 @@ html_theme = 'sphinx_rtd_theme'
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'h', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'r', 'sv', 'tr'
-#html_search_language = 'en'
+html_search_language = 'zh'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
